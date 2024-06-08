69a8329855d91b051c8ace2e4188072052d8daa1
Simon Litges <simon.litges@stud.hn.de>
2024-06-09 01:19:17 +0200

    metainformation added

--- setup.py
+++ setup.py
@@ -6,8 +6,19 @@ OPTIONS = {
     'argv_emulation': False,
     'iconfile': 'images/icon.icns',
     'packages': ['tkinter', 'customtkinter', 'threading', 'pytube'],
-    'includes': ['tkinter', 'customtkinter', 'threading', 'pytube']
+    'includes': ['tkinter', 'customtkinter', 'threading', 'pytube'],
+    'plist': {
+        'CFBundleName': 'YouTube Downloader',
+        'CFBundleShortVersionString': '1.0.0',
+        'CFBundleVersion': '1.0.0',
+        'CFBundleIdentifier': 'de.simoma.youtubedownloader',
+        'CFBundleDevelopmentRegion': 'Deutsch',
+        'CFBundleDisplayName': 'YouTube Downloader',
+        'NSHumanReadableCopyright': '© 2024 Simon Litges. All rights reserved.',
+        'CFBundleGetInfoString': 'YouTube Downloader 1.0.0',
+        'CFBundleExecutable': 'YouTubeDownloader'
     }
+}
 
 setup(
     app=APP,
